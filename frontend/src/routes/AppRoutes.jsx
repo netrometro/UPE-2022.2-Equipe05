e2fd456d0407e478b61816cae3addd5f44a3831b
rikcav <henriquearau000@gmail.com>
2023-03-04 19:51:29 -0300

    feat: adicionando novas rotas para transações

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -4,6 +4,8 @@ import { Login } from "../pages/Login"
 import { Register } from "../pages/Register"
 import { Private } from "./Private"
 import { Logged } from "./Logged"
+import { Transactions } from "../pages/Transactions"
+import { AddTransaction } from "../pages/AddTransaction"
 
 export function AppRoutes() {
     return(
@@ -12,6 +14,8 @@ export function AppRoutes() {
                 <Route path="/" element={<Private Component={Home}/>}/>
                 <Route path="/login" element={<Logged Component={Login}/>}/>
                 <Route path="/signup" element={<Logged Component={Register}/>}/>
+                <Route path="/transactions" element={<Private Component={Transactions}/>}/>
+                <Route path="/add-transaction" element={<Private Component={AddTransaction}/>}/>
             </Routes>
         </BrowserRouter>
     )
