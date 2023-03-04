0a27a1ff46c8620f1ab6d422cd379a3f3901fa8e
Lucas Monteiro de Melo <lucasmonteiromelo@hotmail.com>
2023-03-04 13:05:40 -0300

    feat: integrando sistema de cadastro com o front

--- frontend/src/pages/Login/index.jsx
+++ frontend/src/pages/Login/index.jsx
@@ -1,17 +1,29 @@
+import axios from 'axios';
+import { useState } from 'react'
 import { Button } from "../../components/Button";
 import { TextInput } from "../../components/TextInput"
 
 export function Login() {
+
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  })
+  
+  const login = async (ev) => {
+    try {
+    } catch {}
+  }
 
     return (
       <div>
         <form>
             <h1>WISEWALLET</h1>
             <label htmlFor="email">E-mail</label>
-            <TextInput id={"email"} type={"email"} name={"email"} placeholder={"Digite seu email"}/>
+            <TextInput id={"email"} type={"email"} name={"email"} placeholder={"Digite seu email"} value={data.email}/>
             <label htmlFor="password">Senha</label>
             <TextInput id={"password"} type={"password"} name={"password"} placeholder={"**************"}/>
-            <Button text={"LOGIN"} type={"submit"}/>
+            <Button text={"LOGIN"} type={"submit"} value={data.password}/>
             <a href="">Cadastre-se</a>
         </form>
       </div>
--- frontend/src/pages/Register/index.jsx
+++ frontend/src/pages/Register/index.jsx
@@ -1,18 +1,31 @@
+import { useEffect, useState } from "react";
 import { Button } from "../../components/Button";
 import { TextInput } from "../../components/TextInput"
+import axios from "axios"
 
 export function Register() {
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  })
+
+  const save = async (ev) => {
+    ev.preventDefault();
+    await axios.post("http://localhost:3001/user", data)
+    alert("Conta criada com sucesso, realize o login!")
+  }
 
     return (
       <div>
-        <form>
+        <form onSubmit={save}>
             <h1>WISEWALLET</h1>
             <label htmlFor="username">Nome</label>
-            <TextInput id={"username"} type={"username"} name={"username"} placeholder={"Digite seu nome"}/>
+            <TextInput id={"username"} type={"username"} name={"username"} placeholder={"Digite seu nome"} value={data.name} onChange={ev => setData({...data, name: ev.target.value})}/>
             <label htmlFor="email">E-mail</label>
-            <TextInput id={"email"} type={"email"} name={"email"} placeholder={"Digite seu email"}/>
+            <TextInput id={"email"} type={"email"} name={"email"} placeholder={"Digite seu email"} value={data.email} onChange={ev => setData({...data, email: ev.target.value})}/>
             <label htmlFor="password">Senha</label>
-            <TextInput id={"password"} type={"password"} name={"password"} placeholder={"**************"}/>
+            <TextInput id={"password"} type={"password"} name={"password"} placeholder={"**************"} value={data.password} onChange={ev => setData({...data, password: ev.target.value})}/>
             <Button text={"CADASTRAR"} type={"submit"}/>
             <a href="">Já tem conta? Realize o login</a>
         </form>
