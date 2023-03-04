959291e22866b77ec00dfded9cc35fd464c94a5f
Lucas Monteiro de Melo <lucasmonteiromelo@hotmail.com>
2023-03-04 15:43:46 -0300

    feat: lendo informações através do token jwt

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,19 +1,34 @@
+import jwt from 'jwt-decode';
 import { Button } from "../../components/Button";
 import { TextInput } from "../../components/TextInput"
 import { NavBar } from "../../components/NavBar"
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios';
 
 export function Home() {
 
   const data = JSON.parse(localStorage.getItem("wisewallet"));
+  const decode = jwt(data.token);
 
+  const getUser = async () => {
+    const user = await axios.get(`http://localhost:3001/user/${decode.id}`, {
+      headers: {
+        Authorization: `Bearer ${data.token}`,
+      }
+    });
+    setUser(user.data)
+  }
+
+  const [user, setUser] = useState({});
+  
   useEffect(() => {
-      console.log(data.token)
-  }, [data])
+    getUser();
+  });
 
     return (
       <div>
         <NavBar/>
+        <span>Olá {user.name}</span>
       </div>
     )
   }
