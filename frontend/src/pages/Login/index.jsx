c60008a91ab9dc5fd3dff220d8aec1dab8e8691f
Lucas Monteiro de Melo <lucasmonteiromelo@hotmail.com>
2023-03-04 13:28:23 -0300

    feat: integrando sistema de autenticação com o front

--- frontend/src/pages/Login/index.jsx
+++ frontend/src/pages/Login/index.jsx
@@ -12,18 +12,23 @@ export function Login() {
   
   const login = async (ev) => {
     try {
-    } catch {}
+      ev.preventDefault();
+      const response = await axios.post("http://localhost:3001/login", data);
+      console.log(response?.data)
+    } catch(e) {
+      console.log(e)
+    }
   }
 
     return (
       <div>
-        <form>
+        <form onSubmit={login}>
             <h1>WISEWALLET</h1>
             <label htmlFor="email">E-mail</label>
-            <TextInput id={"email"} type={"email"} name={"email"} placeholder={"Digite seu email"} value={data.email}/>
+            <TextInput id={"email"} type={"email"} name={"email"} placeholder={"Digite seu email"} value={data.email} onChange={ev => setData({...data, email: ev.target.value})}/>
             <label htmlFor="password">Senha</label>
-            <TextInput id={"password"} type={"password"} name={"password"} placeholder={"**************"}/>
-            <Button text={"LOGIN"} type={"submit"} value={data.password}/>
+            <TextInput id={"password"} type={"password"} name={"password"} placeholder={"**************"} value={data.password} onChange={ev => setData({...data, password: ev.target.value})}/>
+            <Button text={"LOGIN"} type={"submit"}/>
             <a href="">Cadastre-se</a>
         </form>
       </div>
