8699990aef9e240c8066770ae2d7cd69d685e948
rikcav <henriquearau000@gmail.com>
2023-03-05 19:53:20 -0300

    feat: buscar transação por id de usuário e por tipo

--- backend/src/routes/transaction_routes.js
+++ backend/src/routes/transaction_routes.js
@@ -1,6 +1,8 @@
-const { create, getByUserId } = require("../controllers/transaction_controller")
+const { create, getByUserId, getUserIdType } = require("../controllers/transaction_controller")
+const { verifyToken } = require("../middlewares/auth_middleware")
 
 exports.transactionRoutes = app => {
   app.post("/transaction", create)
-  app.get("/transaction/:userId", getByUserId)
+  app.get("/transaction/:userId", verifyToken, getByUserId)
+  app.get("/transaction/:userId/:type", verifyToken, getUserIdType)
 }
