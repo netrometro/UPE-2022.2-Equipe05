87da7ba42eafb54f6628f3e03d13c159dc2a758e
rikcav <henriquearau000@gmail.com>
2023-03-05 18:27:12 -0300

    fix: corrigindo rota de transação

--- backend/src/routes/transaction_routes.js
+++ backend/src/routes/transaction_routes.js
@@ -2,5 +2,5 @@ const { create, getByUserId } = require("../controllers/transaction_controller")
 
 exports.transactionRoutes = app => {
   app.post("/transaction", create)
-  app.get("/transaction/:id", getByUserId)
+  app.get("/transaction/:userId", getByUserId)
 }
