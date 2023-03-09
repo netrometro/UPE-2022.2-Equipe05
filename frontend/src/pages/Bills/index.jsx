cb84a61910d59ad76ebb614dc329bb4f6f47be1a
Lucas Monteiro <77646992+lucas752@users.noreply.github.com>
2023-03-09 12:00:40 -0300

    feat: Adicionando funcionalidade de listar e apagar contas na tabela

--- frontend/src/pages/Bills/index.jsx
+++ frontend/src/pages/Bills/index.jsx
@@ -1,17 +1,53 @@
-import { NavBar } from "../../components/NavBar"
-import { Button } from "../../components/Button"
-import "./styles.css"
+import { NavBar } from "../../components/NavBar";
+import { Button } from "../../components/Button";
+import "./styles.css";
 import { Link } from "react-router-dom";
+import jwt from 'jwt-decode';
+import axios from 'axios';
+import { useEffect, useState } from 'react'
 
 export function Bills() {
+  const data = JSON.parse(localStorage.getItem("wisewallet"));
+  const decode = jwt(data.token);
+
+  const getBills = async () => {
+    const bill = await axios.get(`http://localhost:3001/bill/${decode.id}`, {
+      headers: {
+        Authorization: `Bearer ${data.token}`,
+      },
+    });
+    setBill(bill.data);
+  };
+
+  const deleteBill = async (id) => {
+    await axios.delete(`http://localhost:3001/bill/${id}`, {
+      headers: {
+        Authorization: `Bearer ${data.token}`,
+      },
+    });
+    getBills();
+  };
+
+  const [bill, setBill] = useState({});
+
+  useEffect(() => {
+    getBills();
+  }, []);
+
+  const getBillSum = (arr) => {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) sum += arr[i].value;
+    return sum;
+  };
+
   return (
     <div className="bills">
       <div>
-        <NavBar/>
+        <NavBar />
       </div>
       <div className="card-bill">
         <span className="bill-name">CONTAS:</span>
-        <span>R$valor</span>
+        <span>R${getBillSum(bill)}</span>
       </div>
       <div>
         <table>
@@ -25,22 +61,28 @@ export function Bills() {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td><button className="pay-button">Pagar</button></td>
+            {
+              bill?.length ? bill.map((data, index) =>
+            <tr key={index}>
+              <td>{data.title}</td>
+              <td>{data.category}</td>
+              <td>{data.description}</td>
+              <td>R${data.value}</td>
+              <td>{data.dueDate}</td>
+              <td>
+                <button className="pay-button" onClick={() => deleteBill(data.id)}>Pagar</button>
+              </td>
             </tr>
+              ) : null
+            }
           </tbody>
         </table>
       </div>
       <div>
         <Link to={"/add-bill"}>
-          <Button text={"Adicionar"}/>
+          <Button text={"Adicionar"} />
         </Link>
       </div>
     </div>
-  )
+  );
 }
