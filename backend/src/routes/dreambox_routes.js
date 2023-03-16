be4e7ac71d0c13b5c58a906e3038dc6d05833272
Lucas Monteiro <77646992+lucas752@users.noreply.github.com>
2023-03-16 16:39:05 -0300

    feat: protegendo as rotas da dreambox

--- backend/src/routes/dreambox_routes.js
+++ backend/src/routes/dreambox_routes.js
@@ -1,9 +1,10 @@
 const { create, deleteById, getById, getByUserId, update } = require("../controllers/dreambox_controller");
+const { verifyToken } = require ("../middlewares/auth_middleware")
 
 exports.dreamBoxRoutes = app => {
     app.post("/dreambox", create);
-    app.get("/dreamboxes/:userId", getByUserId);
-    app.delete("/dreambox/:id", deleteById)
-    app.put("/dreambox/:id", update)
-    app.get("/dreambox/:id", getById)
+    app.get("/dreamboxes/:userId", verifyToken, getByUserId);
+    app.delete("/dreambox/:id", verifyToken, deleteById)
+    app.put("/dreambox/:id", verifyToken, update)
+    app.get("/dreambox/:id", verifyToken, getById)
 }
