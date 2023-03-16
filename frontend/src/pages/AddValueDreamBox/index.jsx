fdb848d9a54df43b20427057383c54cd19bf38fb
Lucas Monteiro <77646992+lucas752@users.noreply.github.com>
2023-03-16 18:53:16 -0300

    feat: Adicionando página de adicionar valor a caixinha e configurando rotas

---
+++ frontend/src/pages/AddValueDreamBox/index.jsx
@@ -0,0 +1,26 @@
+import { NavBar } from "../../components/NavBar";
+import { Button } from "../../components/Button";
+import { TextInput } from "../../components/TextInput";
+import { useLocation, useNavigate } from "react-router-dom";
+import "./styles.css";
+import { Link } from "react-router-dom";
+import jwt from 'jwt-decode';
+import axios from 'axios';
+import { useEffect, useState } from 'react'
+
+export function AddValueDreamBox() {
+    return(
+        <div className="add-value-dreambox-box">
+            <NavBar/>
+            <div className="add-value-dreambox-title">
+                <span>Adicione um valor a caixinha&nbsp;</span>
+                <span className="add-value-dreambox-name">Caixinha</span>
+            </div>
+            <form action="">
+                <label>Valor a ser adicionado</label>
+                <TextInput/>
+                <Button text={"Adicionar"}/>
+            </form>
+        </div>
+    )
+}
--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -10,6 +10,7 @@ import { Bills } from "../pages/Bills"
 import { AddBill } from "../pages/AddBill"
 import { Planning } from "../pages/Planning"
 import { AddDreamBox } from "../pages/AddDreamBox"
+import { AddValueDreamBox } from "../pages/AddValueDreamBox"
 
 export function AppRoutes() {
     return(
@@ -24,6 +25,7 @@ export function AppRoutes() {
                 <Route path="/add-bill" element={<Private Component={AddBill}/>}/>
                 <Route path="/planning" element={<Private Component={Planning}/>}/>
                 <Route path="/add-dreambox" element={<Private Component={AddDreamBox}/>}/>
+                <Route path="/add-value-dreambox" element={<Private Component={AddValueDreamBox}/>}/>
             </Routes>
         </BrowserRouter>
     )
