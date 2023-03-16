ed43a8d6520cce4948aa55353110f5076acc7f9a
Lucas Monteiro <77646992+lucas752@users.noreply.github.com>
2023-03-16 15:59:06 -0300

    feat: adicionando repository da dreambox

---
+++ backend/src/repositories/dreambox_repository.js
@@ -0,0 +1,41 @@
+const { prisma } = require("../services/prisma")
+
+exports.createDreambox = async (data) => {
+    const dreambox = await prisma.dreamBox.create({data});
+    return dreambox;
+}
+
+exports.getByUserId = async (userId) => {
+    const dreambox = await prisma.dreamBox.findMany({
+        where: {
+            userId
+        }
+    })
+    return dreambox;
+}
+
+exports.getById = async (id) => {
+    const dreambox = await prisma.dreamBox.findUnique({
+        where: {
+            id
+        }
+    })
+}
+
+exports.deleteById = async (id) => {
+    const dreambox = await prisma.dreamBox.delete({
+        where: {
+            id
+        }
+    });
+    return;
+}
+
+exports.update = async (id, data) => {
+    const dreambox = await prisma.dreamBox.update({
+        where: {
+            id
+        },
+        data
+    });
+}
