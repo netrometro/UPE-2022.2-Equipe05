4fe1b91515c0c4f83597d702e3efcc7ddb2cbfab
Lucas Monteiro <77646992+lucas752@users.noreply.github.com>
2023-03-16 17:04:23 -0300

    feat: criando a caixinha na tela de planejamento

--- frontend/src/pages/Planning/index.jsx
+++ frontend/src/pages/Planning/index.jsx
@@ -7,10 +7,46 @@ import axios from 'axios';
 import { useEffect, useState } from 'react'
 
 export function Planning() {
+    const data = JSON.parse(localStorage.getItem("wisewallet"));
+    const decode = jwt(data.token)
+
+    const navigate = useNavigate();
+
+    const getDreamBox = async () => {
+        const dreambox = await axios.get(`http://localhost:3001/dreamboxes/${decode.id}`, {
+          headers: {
+            Authorization: `Bearer ${data.token}`,
+          }
+        });
+        console.log(dreambox.data)
+        setDreambox(dreambox.data);
+    }
+
+    const [dreambox, setDreambox] = useState({});
+
+    useEffect(() => {
+        getDreamBox();
+    }, [])
+
     return(
-        <div>
+        <div className="planning">
             <NavBar/>
-            <span>Planejamento</span>
+            <div className="dreambox-box">
+                <span className="dreambox-title">Planejamento</span>
+                <div className="dreambox">
+                    {
+                        dreambox?.length ? dreambox.map((data, index) =>
+                            <div className="dreambox-card" key={index}>
+                                <span className="dreambox-card-title">{data.name}:&ensp;</span>
+                                <span>R${data.current}</span>
+                                <span>R${data.goal}</span>
+                                <button onClick={() => navigate("/add-value-dreambox", {state: {id: data.id}})}>Adicionar</button>
+
+                            </div>
+                        ) : <span>Nenhuma caixinha cadastrada...</span>
+                    }
+                </div>
+            </div>
         </div>
     )
 }
