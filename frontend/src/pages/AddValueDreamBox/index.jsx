5f784be5f10d9ab75fffab97d9a63572e8f005c6
Lucas Monteiro <77646992+lucas752@users.noreply.github.com>
2023-03-16 19:16:06 -0300

    feat: finalizando a página de adicionar valores a caixinha

--- frontend/src/pages/AddValueDreamBox/index.jsx
+++ frontend/src/pages/AddValueDreamBox/index.jsx
@@ -9,18 +9,68 @@ import axios from 'axios';
 import { useEffect, useState } from 'react'
 
 export function AddValueDreamBox() {
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    const userData = JSON.parse(localStorage.getItem("wisewallet"));
+    const decode = jwt(userData.token);
+    const dreamBoxId = location?.state?.id
+
+    const getDreamBoxById = async () => {
+        try {
+            const dreamBox = await axios.get(`http://localhost:3001/dreambox/${dreamBoxId}`, {
+            headers: {
+                Authorization: `Bearer ${userData.token}`,
+            }
+            });
+            setDreamBoxById(dreamBox.data)
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    const [dreamBoxById, setDreamBoxById] = useState({});
+
+    const [data, setData] = useState({
+        userId: decode.id,
+        name: dreamBoxById.name,
+        current: dreamBoxById.current,
+        goal: dreamBoxById.goal,
+        isActive: dreamBoxById.isActive
+    })
+
+    const updateDreamBox = async (ev) => {
+        try {
+            ev.preventDefault();
+            await axios.put(`http://localhost:3001/dreambox/${location?.state?.id}`, data, {
+                headers: {
+                    Authorization: `Bearer ${userData.token}`,
+                }
+            })
+            alert("Valor adicionado com sucesso");
+            navigate("/planning")
+        } catch (e) {
+            ev.preventDefault();
+            alert("Ocorreu um erro! Tente novamente mais tarde.")
+        }
+    }
+
+    useEffect(() => {
+        getDreamBoxById();
+    },[]);
+
     return(
         <div className="add-value-dreambox-box">
             <NavBar/>
             <div className="add-value-dreambox-form">
                 <div className="add-value-dreambox-title">
                     <span>Adicione um valor a caixinha&nbsp;</span>
-                    <span className="add-value-dreambox-name">Caixinha</span>
+                    <span className="add-value-dreambox-name">{dreamBoxById.name}</span>
                 </div>
-                <form action="">
+                <form onSubmit={updateDreamBox}>
                     <label>Valor a ser adicionado</label>
-                    <TextInput/>
-                    <Button text={"Adicionar"}/>
+                    <TextInput type={"number"} value={data.value} onChange={ev => setData({...data, current: parseFloat(ev.target.value)  + dreamBoxById.current})}/>
+                    <Button text={"Adicionar"} type="submit"/>
                 </form>
             </div>
         </div>
