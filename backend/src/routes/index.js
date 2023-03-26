c6ed0d1d847395cb053a728362e36625362212c5
rikcav <henriquearau000@gmail.com>
2023-03-26 18:25:44 -0300

    feat: novas rotas atualizadas

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,8 +1,9 @@
 const { userRoutes } = require("./user_routes");
 const { authRoutes } = require("./auth_routes");
 const { transactionRoutes } = require("./transaction_routes");
-const { billRoutes } = require("./bill_routes") ;
-const { dreamBoxRoutes } = require("./dreambox_routes")
+const { billRoutes } = require("./bill_routes");
+const { dreamBoxRoutes } = require("./dreambox_routes");
+const { planRoutes } = require("./plan_routes");
 
 module.exports = (app) => {
     userRoutes(app);
@@ -10,4 +11,5 @@ module.exports = (app) => {
     transactionRoutes(app);
     billRoutes(app);
     dreamBoxRoutes(app);
+    planRoutes(app);
 };
