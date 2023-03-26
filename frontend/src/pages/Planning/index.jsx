741b84627327d88bf2cdb74639792d7e97d1c070
rikcav <henriquearau000@gmail.com>
2023-03-26 18:28:29 -0300

    Card de adicionar planejamento

--- frontend/src/pages/Planning/index.jsx
+++ frontend/src/pages/Planning/index.jsx
@@ -64,6 +64,50 @@ export function Planning() {
         getDreamBox();
     }, [])
 
+    const getPlan = async () => {
+        const plan = await axios.get(`http://localhost:3001/plans/${decode.id}`, {
+          headers: {
+            Authorization: `Bearer ${userData.token}`,
+          }
+        });
+        setPlan(plan.data);
+    }
+
+    const updatePlan = async (id) => {
+        try {
+            console.log(typeof(id))
+            await axios.put(`http://localhost:3001/plan/${id}`, data, {
+                headers: {
+                    Authorization: `Bearer ${userData.token}`,
+                }
+            })
+            alert("Planejamento finalizado!");
+            getPlan();
+        } catch (e) {
+            alert("Ocorreu um erro, tente novamente mais tarde!")
+        }
+    }
+
+    const deletePlan = async (id) => {
+        try {
+            await axios.delete(`http://localhost:3001/plan/${id}`, {
+              headers: {
+                Authorization: `Bearer ${userData.token}`,
+              },
+            });
+            alert("Planejamento deletado!")
+            getPlan();
+        } catch (e) {
+            alert("Ocorreu um erro, tente novamente mais tarde!")
+        }
+    };
+
+  const [plan, setPlan] = useState({});
+
+  useEffect(() => {
+    getPlan();
+  }, [])
+
     return(
         <div className="planning">
             <NavBar/>
@@ -93,6 +137,35 @@ export function Planning() {
                 </div>
                 <Link to="/add-dreambox">
                     <Button text={"Nova caixinha"}></Button>
+                </Link>
+            </div>
+
+            <div className="dreambox-box">
+                <span className="dreambox-title">Planejamento</span>
+                <div className="dreambox">
+                    {
+                        plan?.length ? plan.map((data, index) =>
+                            <div className="dreambox-card" key={index}>
+                                <div>
+                                    <span className="dreambox-card-title">{data.name}:&ensp;</span>
+                                    <span>{formatMoney(data.current)}/</span>
+                                    <span>{formatMoney(data.goal)}</span>
+                                </div>
+                                {data.isActive ? 
+                                    <div>
+                                        <button onClick={() => navigate("/add-value-plan", {state: {id: data.id}})}>Adicionar</button>
+                                        <button onClick={() => updatePlan(data.id)}>Finalizar</button>
+                                    </div> : 
+                                    <div>
+                                        <div className="dreambox-finished">Finalizado</div>
+                                        <button onClick={() => deletePlan(data.id)}>Excluir</button>
+                                    </div>}
+                            </div>
+                        ) : <span>Nenhum planejamento cadastrado...</span>
+                    }
+                </div>
+                <Link to="/add-plan">
+                    <Button text={"Novo planejamento"}></Button>
                 </Link>
             </div>
         </div>
