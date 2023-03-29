cf62dbb8651efa3518800708bab3b3c4fdf164aa
Lucas Monteiro <77646992+lucas752@users.noreply.github.com>
2023-03-29 20:57:35 -0300

    feat: adicionando card das transações

--- frontend/src/pages/Transactions/index.jsx
+++ frontend/src/pages/Transactions/index.jsx
@@ -1,20 +1,14 @@
 import { NavBar } from "../../components/NavBar"
+import { CardTransaction } from "../../components/CardTransaction"
 import "./style.css"
 
 export function Transactions() {
   return (
     <div className="transactions">
       <NavBar/>
-      <div>
-        <h1>Transações</h1>
-        <div className="card">
-          <div className="type">
-            Receita
-          </div>
-          <span className="title">Título</span>
-          <span className="description">descricaodescricaodescricaodescricaodescricaodescricaodescricaodescricaodescricaodescricaodescricaodescricaodescricao</span>
-          <span className="value">Valor</span>
-        </div>
+      <div className="transaction-body">
+        <div>Transações</div>
+        <CardTransaction></CardTransaction>
       </div>
     </div>
   )
