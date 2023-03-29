bcbc145fe3241d71a7378111f135231d2b36ef31
Lucas Monteiro <77646992+lucas752@users.noreply.github.com>
2023-03-29 14:44:35 -0300

    feat: adicionando o token de verificação da rota de deletar

--- backend/src/routes/transaction_routes.js
+++ backend/src/routes/transaction_routes.js
@@ -5,5 +5,5 @@ exports.transactionRoutes = app => {
   app.post("/transaction", create)
   app.get("/transaction/:userId", verifyToken, getByUserId)
   app.get("/transaction/:userId/:type", verifyToken, getUserIdType)
-  app.delete("/transaction/:id", deleteById)
+  app.delete("/transaction/:id", verifyToken, deleteById)
 }
